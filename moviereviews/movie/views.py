1115732a175bf0f17a2349f364cb385608b50962
Neha Awasthi <nawasthi34@123>
2022-02-08 11:28:16 +0530

    session4 Form, urls

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'swarnita'})
+    search_bar = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'search_bar': search_bar})
 
 
 def about(request):
     return HttpResponse("this is your about us page.")
+
+
+def signup(request):
+    print(request)
+    email = request.GET['signupEmail']
+    return render(request, 'singup.html', {'email': email})
--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mv.home),
     path('about/', mv.about),
+    path('signup/', mv.signup, name='signup'),
 ]
