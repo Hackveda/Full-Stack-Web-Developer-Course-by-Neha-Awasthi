0ec41566515648941545c7f73e7fdb913aa9f3fe
Neha Awasthi <nawasthi34@123>
2022-02-11 13:42:05 +0530

    movie listing

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
     search_bar = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'search_bar': search_bar})
+    if search_bar:
+        query_set = Movie.objects.filter(title__icontains=search_bar)
+    else:
+        query_set = Movie.objects.all()
+    return render(request, 'home.html', {'search_bar': search_bar,'movie':query_set})
 
 
 def about(request):
