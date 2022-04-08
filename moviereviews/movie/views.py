4ac91b7739729e2e7a752a41e998da5beab4aefd
Neha Awasthi <nawasthi34@gmail.com>
2022-04-08 08:59:47 +0530

    remove space

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -12,7 +12,6 @@ def home(request):
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-
     return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 
@@ -23,7 +22,6 @@ def about(request):
 
 def signup(request):
     email = request.GET.get('email')
-
     return render(request, 'signup.html', {'email': email})
 
 
